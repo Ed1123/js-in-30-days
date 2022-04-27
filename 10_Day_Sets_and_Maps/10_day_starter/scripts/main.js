ffbe9e13a1efbea26a7b37e92ce5d1b606625191
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-04-26 21:48:05 -0500

    set

--- 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
+++ 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
@@ -1,2 +1,13 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// console.log(countries)
+// alert('Open the console and check if the countries has been loaded')
+
+const langs = new Set()
+
+countries.forEach(country => {
+    country['languages'].forEach(language => {
+        langs.add(language)
+    })
+})
+
+console.log(langs)
+console.log(langs.size)
