7505f396a6a2dfeb7a092db4cd6cec4d44d70a07
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-04-28 19:35:21 -0500

    Destructuring

--- 11_Day_Destructuring_and_spreading/11_day_starter/scripts/main.js
+++ 11_Day_Destructuring_and_spreading/11_day_starter/scripts/main.js
@@ -1,2 +1,27 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// console.log(countries)
+// alert('Open the console and check if the countries has been loaded')
+
+const students = [
+    ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+    ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+]
+
+function convertArrayToObject(array) {
+    const r = []
+    for ([name, skills, scores] of array) {
+        r.push(
+            {
+                name: name,
+                skills: skills,
+                scores: scores
+            }
+        )
+    }
+    return r
+}
+
+console.log(convertArrayToObject(students))
+
+let student = convertArrayToObject(students)[0]
+
+student
